7c4d13c3f2e0eb065a08bdd0025d91f4558fb90a
sahanigai <sahanigai2017@gmail.com>
2025-03-03 23:51:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+class Book:
+    """Базовый класс книги."""
+
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    """Класс бумажной книги."""
+
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)  # Используем конструктор базового класса
+        self.pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages})"
+
+
+class AudioBook(Book):
+    """Класс аудиокниги."""
+
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)  # Используем конструктор базового класса
+        self.duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration})"
